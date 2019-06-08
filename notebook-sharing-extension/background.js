501683d5e0ff359b84a92ccaf53e5ba8fe9cce11
Nathaniel Verhaaren <nverhaaren94@gmail.com>
2019-06-08 17:05:32 -0500

    remove popup fixing, style * could not test due to lds->churchofjesuschrist

--- notebook-sharing-extension/background.js
+++ notebook-sharing-extension/background.js
@@ -1,13 +1,7 @@
 let loggedIn = false;
-let popupsFixed = false;
 
 function getLoggedIn() {
   return loggedIn;
-}
-
-function unfixPopups() {
-  console.log("unfixPopups");
-  popupsFixed = false;
 }
 
 function updatePopup(newLoggedIn) {
@@ -15,65 +9,33 @@ function updatePopup(newLoggedIn) {
     return;
   }
   if (newLoggedIn) {
-    console.log("logged in");
+    console.log('logged in');
     loggedIn = true;
-    if (!popupsFixed) {
-      console.log("set popup to main");
-      chrome.browserAction.setPopup({popup: "main_popup.html"});
-    }
+    console.log('set popup to main');
+    chrome.browserAction.setPopup({popup: 'main_popup.html'});
   } else {
-    console.log("logged out");
+    console.log('logged out');
     loggedIn = false;
-    if (!popupsFixed) {
-      console.log("set popup to login");
-      chrome.browserAction.setPopup({popup: "login_popup.html"});
-    }
+    console.log('set popup to login');
+    chrome.browserAction.setPopup({popup: 'login_popup.html'});
   }
 }
 
 chrome.cookies.onChanged.addListener(changeInfo => {
   const cookie = changeInfo.cookie;
-  console.debug("Processing cookie:");
+  console.debug('Processing cookie:');
   // console.debug(cookie);
-  if (cookie.name === "lds-id" && cookie.domain === ".lds.org") {
+  if (cookie.name === 'lds-id' && cookie.domain === '.lds.org') {
     console.debug('Processing lds-id cookie');
     updatePopup(!(changeInfo.removed));
   }
 });
-console.debug("added cookie listener");
+console.debug('added cookie listener');
 
 chrome.cookies.get({url: 'https://www.lds.org', name: 'lds-id'}, cookie => {
   if (cookie !== null) {
-    console.log("Found lds-id cookie on startup");
-    loggedIn = true;
-    chrome.browserAction.setPopup({popup: "main_popup.html"});
-    console.log("set popup to main");
-  }
-});
-
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.type === "getLoggedIn") {
-    console.debug("sending loggedIn to popup");
-    sendResponse(loggedIn);
-  } else if (request.type === "fixPopups") {
-    console.log("fixing popups");
-    popupsFixed = true;
-    sendResponse(true);
-  } else if (request.type === "unfixPopups") {
-    console.log("unfixing popups");
-    popupsFixed = false;
-    chrome.cookies.get({url: 'https://www.lds.org', name: 'lds-id'}, cookie => {
-      if (cookie === null) {
-        console.log("We are now logged out");
-        // loggedIn should already be true
-        console.debug(loggedIn);
-        chrome.browserAction.setPopup({popup: "login_popup.html"});
-      }
-    });
-    sendResponse(true);
-  } else {
-    console.log("Received request of unknown type:");
-    console.log(request);
-    console.log(sender);
+    console.log('Found lds-id cookie on startup');
+    updatePopup(true);
+    console.log('set popup to main');
   }
 });
--- notebook-sharing-extension/login_popup.js
+++ notebook-sharing-extension/login_popup.js
@@ -1,10 +1,10 @@
-const openLogin = document.getElementById("openLogin");
-console.log("In login_popup.js");
+const openLogin = document.getElementById('openLogin');
+console.log('In login_popup.js');
 
-openLogin.onclick = function(element) {
-  console.debug("About to open tab");
+openLogin.onclick = element => {
+  console.debug('About to open tab');
   chrome.tabs.create({
-    url: "https://signin.lds.org/signinRedirect?goto=https%3a%2f%2fwww.lds.org%2f",
+    url: 'https://signin.lds.org/signinRedirect?goto=https%3a%2f%2fwww.lds.org%2f',
     active: true,
   });
 };
